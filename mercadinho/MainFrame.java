0eb73752e29201835edae6128fbed220f8a34de2
ceconelli <guceconelli@hotmail.com>
2017-11-15 14:54:27 -0200

    add Producer management - incomplet

--- mercadinho/MainFrame.java
+++ mercadinho/MainFrame.java
@@ -30,26 +30,22 @@ public class MainFrame extends javax.swing.JFrame {
     
     public MainFrame(List<Producer> producers) {
         initComponents();
-        Producer[] pd = new Producer[producers.size()];
         
+        Producer[] pd = new Producer[producers.size()];
+        DefaultListModel model = new DefaultListModel();
         int i = 0;
+        
         for(Producer p:producers){
             System.out.println(p.getCpf());
+            model.addElement(p);
             pd[i] = p;
             i++;
         }
         
         this.cb_Producer.setModel(new DefaultComboBoxModel(pd));
         
-        DefaultListModel model = new DefaultListModel();
-        model.addElement(producers.get(0));
-        model.addElement(producers.get(1));
-        model.addElement(producers.get(2));
         this.list_producers.setModel(model);
-//        this.list_producers = new JList(model);
-        ListModel l;
-//        this.list_producers.setModel(new DefaultListModel<>());
-        
+
         prod = producers;
         
     }
@@ -86,10 +82,12 @@ public class MainFrame extends javax.swing.JFrame {
         list_producers = new javax.swing.JList<>();
         jLabel4 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        tf_name = new javax.swing.JTextField();
         jLabel9 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
+        tf_id = new javax.swing.JTextField();
         btn_addProducer = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList<>();
         jPanel6 = new javax.swing.JPanel();
         jPanel7 = new javax.swing.JPanel();
 
@@ -104,6 +102,11 @@ public class MainFrame extends javax.swing.JFrame {
         tf_ProducerID.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusLost(java.awt.event.FocusEvent evt) {
                 tf_ProducerIDFocusLost(evt);
+            }
+        });
+        tf_ProducerID.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                tf_ProducerIDKeyTyped(evt);
             }
         });
 
@@ -223,19 +226,22 @@ public class MainFrame extends javax.swing.JFrame {
 
         jTabbedPane1.addTab("Vendas", jPanel2);
 
+        list_producers.addListSelectionListener(new javax.swing.event.ListSelectionListener() {
+            public void valueChanged(javax.swing.event.ListSelectionEvent evt) {
+                list_producersValueChanged(evt);
+            }
+        });
         jScrollPane1.setViewportView(list_producers);
 
         jLabel4.setText("Produtores");
 
         jLabel8.setText("Nome:");
 
-        jTextField1.setText("jTextField1");
-
         jLabel9.setText("Código:");
 
-        jTextField2.setText("jTextField1");
-
         btn_addProducer.setText("Add Produtor");
+
+        jScrollPane2.setViewportView(jList1);
 
         javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
         jPanel5.setLayout(jPanel5Layout);
@@ -245,18 +251,19 @@ public class MainFrame extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                     .addComponent(btn_addProducer, javax.swing.GroupLayout.DEFAULT_SIZE, 132, Short.MAX_VALUE)
-                    .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
-                        .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, 132, Short.MAX_VALUE)))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel8)
                     .addComponent(jLabel9))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(553, Short.MAX_VALUE))
+                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(tf_id, javax.swing.GroupLayout.DEFAULT_SIZE, 86, Short.MAX_VALUE)
+                    .addComponent(tf_name))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 535, Short.MAX_VALUE)
+                .addContainerGap())
         );
         jPanel5Layout.setVerticalGroup(
             jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -264,19 +271,22 @@ public class MainFrame extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(jLabel4)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(jPanel5Layout.createSequentialGroup()
-                        .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel8)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel9)
-                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 233, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btn_addProducer)
-                .addContainerGap())
+                        .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel5Layout.createSequentialGroup()
+                                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                    .addComponent(jLabel8)
+                                    .addComponent(tf_name, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                    .addComponent(jLabel9)
+                                    .addComponent(tf_id, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 233, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btn_addProducer))
+                    .addComponent(jScrollPane2))
+                .addGap(119, 119, 119))
         );
 
         jTabbedPane2.addTab("Produtores", jPanel5);
@@ -398,6 +408,20 @@ public class MainFrame extends javax.swing.JFrame {
         this.label_price.setText(""+p.getPrice());
         System.out.println("entrou");
     }//GEN-LAST:event_cb_productItemStateChanged
+
+    private void list_producersValueChanged(javax.swing.event.ListSelectionEvent evt) {//GEN-FIRST:event_list_producersValueChanged
+        // TODO add your handling code here:
+        Producer p = this.prod.get(this.list_producers.getSelectedIndex());
+        this.tf_name.setText(p.getName());
+        this.tf_id.setText(p.getId());
+        
+    }//GEN-LAST:event_list_producersValueChanged
+
+    private void tf_ProducerIDKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_tf_ProducerIDKeyTyped
+        // TODO add your handling code here:
+        
+        System.out.println(""+evt.getKeyChar());
+    }//GEN-LAST:event_tf_ProducerIDKeyTyped
     
     private Object[] getObjs(List<Object> list){
         Object[] objs = new Object[list.size()];
@@ -469,6 +493,7 @@ public class MainFrame extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
+    private javax.swing.JList<String> jList1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
@@ -476,15 +501,16 @@ public class MainFrame extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel6;
     private javax.swing.JPanel jPanel7;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JSpinner jSpinner1;
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JTabbedPane jTabbedPane2;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
     private javax.swing.JLabel label_availableAmount;
     private javax.swing.JLabel label_price;
     private javax.swing.JList<String> list_producers;
     private javax.swing.JTextField tf_ProducerID;
+    private javax.swing.JTextField tf_id;
+    private javax.swing.JTextField tf_name;
     // End of variables declaration//GEN-END:variables
 
 }
